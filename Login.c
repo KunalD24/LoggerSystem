b0486b0e86f0f30ae0ff815e535244f69edff08a
Kunal <93595799+KunalD24@users.noreply.github.com>
2025-08-01 12:04:25 +0530

    Update Login.c

--- Login.c
+++ Login.c
@@ -59,66 +59,99 @@ int  is_palindrome(char *str)
 int main()
 {
     char *username = (char *)malloc(101);
+    char *dummyUsername = (char *)malloc(101); 
     char *fileBuffer = (char *)malloc(1000);
+
     fileBuffer[0] = '\0';
     int choice;
-
-    printf("Please Enter the Username : \n");
-    fgets(username, 101, stdin);
-    sprintf(fileBuffer + strlen(fileBuffer),"Username is %s",username);
-    
-
-    int len = strlen(username);
-    if(len > 0 && username[len - 1]=='\n')
+    while(1)
     {
-        username[len-1]='\0';
-    }
-    do
-    {
-        printf("\nChoose an operation:\n");
-        printf("1. Uppercase\n");
-        printf("2. Lowercase\n");
-        printf("3. Palindrome\n");
-        printf("4. Reverse\n");
-        printf("5. Exit\n");
-        printf("Enter your choice (1-5): ");
-        scanf("%d", &choice);
-        switch(choice)
+        while(1)
         {
-            case 1:
-                to_uppercase(username);
-                printf("Uppercase: %s\n", username);
-                sprintf(fileBuffer + strlen(fileBuffer),"Userame in uppercase is %s\n",username);
+            printf("Please Enter a Username : ");
+            fgets(username, 101, stdin);
+            int len = strlen(username);
+            if(len > 0 && username[len - 1]=='\n')
+            {
+                username[len-1]='\0';
+            }
+            if (strlen(username) == 0) 
+            {
+                printf("Please enter a valid username.\n");
+            }
+            else
+            {
                 break;
-            case 2:
-                to_lowercase(username);
-                printf("Lowercase: %s\n", username);
-                sprintf(fileBuffer + strlen(fileBuffer),"Userame in lowercase is %s\n",username);
-                break;
-            case 3:
-                if (is_palindrome(username)) 
-                {
-                    printf("%s is a palindrome.\n",username);
-                    sprintf(fileBuffer + strlen(fileBuffer),"Userame %s is palindrome\n",username);
-                } else 
-                {
-                    printf("%s is not a palindrome.\n",username);
-                    sprintf(fileBuffer + strlen(fileBuffer),"Userame %s is not palindrome\n",username);
-                }
-                break;
-            case 4:
-                reverse_string(username);
-                printf("Reversed: %s\n", username);
-                sprintf(fileBuffer + strlen(fileBuffer),"Userame in reverse is %s\n",username);
-                break;
-            case 5:
-                printf("Exiting program.\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
+            }
         }
-    } while(choice!=5);
 
+        strcpy(dummyUsername,username);
+        printf("Username is %s\n",username);
+        sprintf(fileBuffer + strlen(fileBuffer),"\nUsername is %s\n\n",username);
+        // sprintf(fileBuffer + strlen(fileBuffer),"%s\n\n",username);
+        // printf("%s",username);
+    
+        do
+        {
+            printf("\nChoose an operation:\n");
+            printf("1. Uppercase\n");
+            printf("2. Lowercase\n");
+            printf("3. Palindrome\n");
+            printf("4. Reverse\n");
+            printf("5. Exit\n");
+            printf("Enter your choice (1-5): ");
+            scanf("%d", &choice);
+            switch(choice)
+            {
+                case 1:
+                    strcpy(username,dummyUsername);
+                    to_uppercase(username);
+                    printf("Uppercase: %s\n", username);
+                    sprintf(fileBuffer + strlen(fileBuffer),"Userame in uppercase is %s\n",username);
+                    break;
+                case 2:
+                    strcpy(username,dummyUsername);
+                    to_lowercase(username);
+                    printf("Lowercase: %s\n", username);
+                    sprintf(fileBuffer + strlen(fileBuffer),"Userame in lowercase is %s\n",username);
+                    break;
+                case 3:
+                    strcpy(username,dummyUsername);
+                    if (is_palindrome(username)) 
+                    {
+                        printf("%s is a palindrome.\n",username);
+                        sprintf(fileBuffer + strlen(fileBuffer),"Userame %s is palindrome\n",username);
+                    } 
+                    else 
+                    {
+                        printf("%s is not a palindrome.\n",username);
+                        sprintf(fileBuffer + strlen(fileBuffer),"Userame %s is not palindrome\n",username);
+                    }
+                    break;
+                case 4:
+                    strcpy(username,dummyUsername);
+                    reverse_string(username);
+                    printf("Reversed: %s\n", username);
+                    sprintf(fileBuffer + strlen(fileBuffer),"Userame in reverse is %s\n",username);
+                    break;
+                case 5:
+                    break;
+                default:
+                    printf("Invalid choice. Please try again.\n");
+            }
+        } 
+        while(choice!=5);
+        printf("\nDo you want to enter another username?\n");
+        printf("1. Yes\n");
+        printf("2. No\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+        if (choice != 1) 
+        {
+            break;
+        }
+    }
     printf("\nYou want to export:\n");
     printf("1. Yes\n");
     printf("2. No\n");
@@ -136,7 +169,7 @@ int main()
             }
             else
             {
-                printf("Failed to Exported");
+                printf("Failed to Export");
             }}
             break;
         case 2:
@@ -146,5 +179,7 @@ int main()
             printf("Invalid choice\n"); 
     }
     free(username);
+    free(fileBuffer);
+    free(dummyUsername);
     return 0;
 }
